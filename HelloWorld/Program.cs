6f18f20ed011b9e57340bd26f50c20466a2ad274
Liang Hongrui <l.hr@qq.com>
2016-04-20 12:32:22 +0800

    add hello world test

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.PrintChat("Hello World, my name is LHR!");
+        }
+    }
+}
